2c2ffab9fcbd59443f13ed42547a087a13458c95
harshit <harshitwadhwani23@gmail.com>
2024-04-19 01:21:48 +0530

    added doc upload and temp result

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, redirect, send_file, url_for,request,jsonify
 from utilities.dbmanager import dbmanager
 from utilities.qpapergenerator import GenerateQpaper
+from utilities.ocrmanager import detect_document_text
 import json
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
     return render_template("index.html")
 
@@ -16,7 +21,7 @@ def qp():
 @app.route('/data', methods=['POST'])
 def save_data():
     data = request.get_json()
-    with open('Answersheet-AI/data/current.json', 'w') as f:
+    with open('data\\current.json', 'w') as f:
         json.dump(data, f)
     return jsonify({'message': 'Data saved successfully'})
 
@@ -25,7 +30,7 @@ def generate():
     db_manager = dbmanager()
     qp_generater= GenerateQpaper()
     collection_name='questions'
-    with open('Answersheet-AI/data/current.json', 'r') as file:
+    with open('data/current.json', 'r') as file:
      json_data = json.load(file)
     db_manager.create(collection_name,json_data)
     qp_generater.MakePaper()
@@ -45,6 +50,39 @@ def get_generate_result():
 @app.route('/download/this.pdf')
 def download_this_pdf():
     return send_file('data/questionpaper.pdf', as_attachment=True)
+
+@app.route("/evaluation", methods=['GET', 'POST'])
+def evaluation():
+    return render_template("evaluation.html")
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file uploaded', 400
+
+    file = request.files['file']
+
+    # Check if the file was actually uploaded
+    if file.filename == '':
+        return 'No file selected', 400
+
+    # Save the file to the upload folder
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+    
+    
+    return redirect(url_for("result", filename=filename))
+
+@app.route("/result/<filename>")
+def result(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    
+    l = detect_document_text(file_path)
+    
+    return render_template('result.html', filename=filename, l=l)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
